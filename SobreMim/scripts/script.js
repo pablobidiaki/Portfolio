e0422c165fdcef77e91dc2ca2175ab284bac483e
pablobidiaki <pablobidiaki@hotmail.com>
2023-01-25 16:17:34 -0300

    sobre mim finalizado

--- SobreMim/scripts/script.js
+++ SobreMim/scripts/script.js
@@ -2,35 +2,34 @@ const burguer = document.getElementById("menu-burguer")
 const closeBurguer = document.getElementById("close-button")
 const lista = document.getElementById("lista")
 const main = document.getElementById("main")
-const body = document.getElementById("body")
+
 
 burguer.addEventListener('click', () => {
     burguer.style.display = "none"
     closeBurguer.style.display = "block"
     main.style.display="none"
-    body.style.background= "linear-gradient(rgba(0, 0, 0, 0.900),rgba(25, 25, 24, 0.900) .100%), url(imgs/fundo.jpg)"
     burguer.style.opacity = "0"
-    lista.classList.add('burguerON')
+    lista.style.display = 'flex'
+    lista.classList.add('burguerON')   
 })
 
 closeBurguer.addEventListener('click', () => {
     closeBurguer.style.display = "none"
     burguer.style.display = "block"
     main.style.display="block"
-    body.style.background= "linear-gradient(rgba(0, 0, 0, 0.5),rgba(25, 25, 24, 0.5) .100%), url(imgs/fundo.jpg)"
-    body.style.backgroundPosition= "center center"
     burguer.style.opacity = "1"
+    lista.style.display = 'none'
     lista.classList.remove('burguerON')
 
     setInterval(bugBurguer, 500)
 })
 
+
 function bugBurguer(){
     var largura = window.innerWidth;
-    if(largura > 820){
+    if(largura > 962){
         burguer.style.display = "none"
     }else {
         burguer.style.display = 'block'
     }
-    console.log(largura)
 }
