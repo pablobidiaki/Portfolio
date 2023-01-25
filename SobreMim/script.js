4806f23968433406cea69c9f2482d69df010e050
pablobidiaki <pablobidiaki@hotmail.com>
2023-01-25 14:28:53 -0300

    dark mode finalizado

--- SobreMim/script.js
+++ SobreMim/script.js
@@ -9,16 +9,34 @@ const body = document.getElementById('body')
 const dLinks = document.getElementById('dark')
 const lLinks = document.getElementById('light')
 
+let contagem = 0
+
 dark.addEventListener('click', () => {
-    dark.innerHTML = "Dark Mode"
-    dark.style.color = "#252525"
-    op1.style.color = "#252525"
-    op2.style.color = "#252525"
-    op3.style.color = "#252525"
-    op4.style.color = "#252525"
-    hello.style.color = "#252525"
-    content.style.color = "#252525"
-    body.style.backgroundColor = "#dfdfdf"
-    lLinks.style.display = 'none'
-    dLinks.style.display = 'flex'
+    contagem = contagem + 1
+    if(contagem % 2 == 1){
+        dark.innerHTML = "Dark Mode"
+        dark.style.color = "#252525"
+        op1.style.color = "#252525"
+        op2.style.color = "#252525"
+        op3.style.color = "#252525"
+        op4.style.color = "#252525"
+        hello.style.color = "#252525"
+        content.style.color = "#252525"
+        body.style.backgroundColor = "#dfdfdf"
+        lLinks.style.display = 'none'
+        dLinks.style.display = 'flex'
+    } else {
+        dark.innerHTML = "Light Mode"
+        dark.style.color = "white"
+        op1.style.color = "white"
+        op2.style.color = "white"
+        op3.style.color = "white"
+        op4.style.color = "white"
+        hello.style.color = "white"
+        content.style.color = "white"
+        body.style.backgroundColor = "#252525"
+        lLinks.style.display = 'flex'
+        dLinks.style.display = 'none'
+    }
 })
+
