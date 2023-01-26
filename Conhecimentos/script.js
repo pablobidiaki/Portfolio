1ab7e27910e4cede3df5a4ce738b407bcbf50d12
pablobidiaki <pablobidiaki@hotmail.com>
2023-01-26 13:21:10 -0300

    Conhecimentos para pc pronto

--- Conhecimentos/script.js
+++ Conhecimentos/script.js
@@ -42,6 +42,8 @@ css.addEventListener('click', () => {
     modal.classList.remove('none')
     main.style.opacity = ".2"
     header.style.opacity = ".2"
+    title.innerHTML = "CSS"
+    details.innerHTML = "Junto com o HTML é uma das tecnologias que mais domino pelo fato de todos os cursos de HTML  o CSS vir acompanhado e assim como ele aprendi tanto na aula de Desenvolvimento Web na Etec como em cursos por fora."
     travarScroll();
 })
 
@@ -50,6 +52,8 @@ js.addEventListener('click', () => {
     modal.classList.remove('none')
     main.style.opacity = ".2"
     header.style.opacity = ".2"
+    title.innerHTML = "Javascript"
+    details.innerHTML = "Uma das linguagens de programação mais utilizadas no mundo, diferente do HTML e CSS não tive contato com essa tecnologia no meu curso técnico e todo conhecimento que possuo é de cursos, vídeos e  pesquisas realizadas pela internet, grande parte dos meus projetos utilizo Javascript."
     travarScroll();
 })
 
@@ -58,6 +62,8 @@ react.addEventListener('click', () => {
     modal.classList.remove('none')
     main.style.opacity = ".2"
     header.style.opacity = ".2"
+    title.innerHTML = "React.JS"
+    details.innerHTML = "A biblioteca React foi o assunto do ultimo curso que realizei e a principal tecnologia que estou realizando projetos estudando mais a fundo. Assim como o javascript não tive contato com ele no meu curso técnico e meus conhecimentos sobre são de cursos, vídeos, pesquisas e projetos pessoais."
     travarScroll();
 })
 
@@ -66,6 +72,8 @@ reactNative.addEventListener('click', () => {
     modal.classList.remove('none')
     main.style.opacity = ".2"
     header.style.opacity = ".2"
+    title.innerHTML = "React Native"
+    details.innerHTML = "Diferete do React e do Javascript o meu primeiro e principais contatos com React Native foram feitos na Etec, na aula de Desenvolvimento Mobile, onde trabalhamos muito com templates prontos que o professor disponibilizava nas aulas fazendo com que aplicássemos isso em algum exercício."
     travarScroll();
 })
 
@@ -74,6 +82,8 @@ php.addEventListener('click', () => {
     modal.classList.remove('none')
     main.style.opacity = ".2"
     header.style.opacity = ".2"
+    title.innerHTML = "PHP"
+    details.innerHTML = "O PHP foi o principal assunto abordado na aula de Desenvolvimento Web II no meu curso técnico, realizei alguns projetos pessoais utilizando essa linguagem, mas a grande maioria e principais projetos eram realizados para as aulas. Com meu desempenho nas aulas eu e mais um colega de turma fomos os únicos selecionados para fazer um projeto representando nosso módulo e curso para a feira tecnologica realizada em nossa escola."
     travarScroll();
 })
 
@@ -82,6 +92,8 @@ sql.addEventListener('click', () => {
     modal.classList.remove('none')
     main.style.opacity = ".2"
     header.style.opacity = ".2"
+    title.innerHTML = "MySQL"
+    details.innerHTML = "Meu conhecimento de Banco de Dados se deve a Etec, onde no primeiro módulo foi abordado os primeiros passos, teorias, e como projeto final a modelagem de um, onde o assunto escolhido pelo meu grupo foi: enciclopédia de inventores. Já no segundo modúlo o foco foi algo mais pratico onde criamos um Banco de dados e manipulavamos os dados com a linguagem SQL."
     travarScroll();
 })
 
@@ -90,6 +102,8 @@ cSharp.addEventListener('click', () => {
     modal.classList.remove('none')
     main.style.opacity = ".2"
     header.style.opacity = ".2"
+    title.innerHTML = "C#"
+    details.innerHTML = "Creio que essa tenha sido a materia mais prática do meu curso até o momento onde o professor que ministrava as aula já tinha trabalhado como desenvolvedor se Software e por esse fato o conhecimento que ele conseguia nos passar era algo muito amplo."
     travarScroll();
 })
 
